660ad68e815d779a9387d1be749eb9c59926f169
pedromends <joaopedromend14@gmail.com>
2025-04-12 23:27:50 -0300

    estrutura corrigida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 @NgModule({
   declarations: [
@@ -12,10 +17,21 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
+    BrowserModule,
     ComponentsModule,
     AppRoutingModule,
     RouterModule,
     PagesModule
-  ]
+  ],
+  bootstrap:[
+    AppComponent
+  ],
+  providers: [
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: { preset: Aura, options: { darkModeSelector: '.p-dark' } },
+    }),
+  ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-import { AppComponent } from './app/app.component';
 
-  export const appConfig: ApplicationConfig = {
-      providers: [
-      provideHttpClient(withFetch()),
-      provideAnimationsAsync(),
-      providePrimeNG({
-          theme: { preset: Aura, options: { darkModeSelector: '.p-dark' } },
-      }),
-    ],
-  };
+import { AppModule } from './app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-    bootstrapApplication(AppComponent, appConfig).catch((err) =>
-    console.error(err)
-);
+  platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err))
+  ;
