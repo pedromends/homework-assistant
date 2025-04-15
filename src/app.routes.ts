3db48f18f82ead74f83cc6590ed9047748811576
pedromends <joaopedromend14@gmail.com>
2025-04-14 21:32:36 -0300

    go

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { AppLayout } from './app/layout/component/app.layout';
-import { Documentation } from './app/pages/documentation/documentation';
-import { Landing } from './app/pages/landing/landing';
-import { Notfound } from './app/pages/notfound/notfound';
-import { DashboardComponent } from './app/pages/dashboard/dashboard.component';
+import { DashboardComponent } from './app/pages/public/dashboard/dashboard.component';
 
 export const appRoutes: Routes = [
     {
@@ -11,13 +8,10 @@ export const appRoutes: Routes = [
         component: AppLayout,
         children: [
             { path: '', component: DashboardComponent },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation },
+            { path: 'uikit', loadChildren: () => import('./app/pages/public/uikit/uikit.routes') },
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
         ]
     },
-    { path: 'landing', component: Landing },
-    { path: 'notfound', component: Notfound },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
+    { path: 'auth', loadChildren: () => import('./app/pages/public/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
 ];
